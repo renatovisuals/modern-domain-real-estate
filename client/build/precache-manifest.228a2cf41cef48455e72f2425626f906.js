f5ff6b696886f85b8cf6d8f0213d4faf7daf3dab
James <renatovisuals@gmail.com>
2019-12-05 15:41:24 -0600

    debugging main listing page

---
+++ client/build/precache-manifest.228a2cf41cef48455e72f2425626f906.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fca291bf0260b4e1872e",
+    "url": "/static/js/main.1145ee42.chunk.js"
+  },
+  {
+    "revision": "1a934109bab5e68e1d0c",
+    "url": "/static/js/2.4bc490d5.chunk.js"
+  },
+  {
+    "revision": "fca291bf0260b4e1872e",
+    "url": "/static/css/main.e083e849.chunk.css"
+  },
+  {
+    "revision": "69a01a6469f36e065480fda634f20a1c",
+    "url": "/index.html"
+  }
+];
