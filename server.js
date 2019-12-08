928d6dbed83c4895860b0ffedaaf22a5fde2e252
James <renatovisuals@gmail.com>
2019-12-08 16:12:21 -0600

    troubleshooting broken production routes

--- server.js
+++ server.js
@@ -37,7 +37,9 @@ app.use('/api/listings', listingsRouter);
 app.use('/api/agents', agentRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,'client/build')));
+  //app.use(express.static('client/build'));
+  //app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get('*',(req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
